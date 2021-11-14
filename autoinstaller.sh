aafe13c2fd3dcbe02d846257e846c11736768048
Hykilpikonna <me@hydev.org>
2021-11-13 23:47:10 -0500

    [+] Create auto install script

---
+++ autoinstaller.sh
@@ -0,0 +1,32 @@
+# https://www.vultr.com/docs/how-to-install-nixos-on-a-vultr-vps
+mkdir ~/.ssh
+curl -L https://github.com/Hykilpikonna.keys >~/.ssh/authorized_keys
+
+sudo -s
+
+parted /dev/vda -s mklabel msdos
+parted /dev/vda -s mkpart primary 1MiB -1GiB
+parted /dev/vda -s mkpart primary linux-swap -1GiB 100%
+
+mkfs.btrfs -L root /dev/vda1
+mkswap -L swap /dev/vda2
+swapon /dev/vda2
+mount /dev/disk/by-label/root /mnt
+
+nixos-generate-config --root /mnt
+rm -rf /mnt/etc/nixos
+cd /mnt/etc
+nix-shell -p git --run 'git clone https://github.com/hykilpikonna/NixOS-Server.git nixos'
+
+cp /home/nixos/.ssh/authorized_keys /mnt/etc/nixos/key.pub
+
+ssh-keygen
+cat /root/.ssh/id_rsa.pub
+
+echo "Copy public key to your github account."
+read -p "Press enter to continue"
+
+nixos-install --no-root-passwd
+
+echo "Done!"
+echo "Please shut down and remove installer iso."
